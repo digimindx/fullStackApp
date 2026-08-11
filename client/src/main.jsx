a2c178a096809e2c0b2ed9ebbc43963e67001adc
digimindx <digimind.x@gmail.com>
2026-08-11 16:18:58 +0300

    clearing

--- client/src/main.jsx
+++
@@ -1,8 +0,0 @@
-
-import { createRoot } from 'react-dom/client'
-import './Assets/css/Index.css';
-import App from './Pages/Page'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
