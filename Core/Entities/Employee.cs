a79397c4ed52ba99c570620dedd0812de675a6b9
digimindx <digimind.x@gmail.com>
2026-08-11 16:49:57 +0300

    add properities

--- Core/Entities/Employee.cs
+++ Core/Entities/Employee.cs
@@ -2,6 +2,8 @@ namespace CORE.Entities
 {
     public class Employee
     {
-        
+        public String FirstName {set; get;}
+        public String MiddleName {set; get;}
+        public String LastName {set; get;}
     }
 }
